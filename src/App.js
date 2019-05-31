e0c78159ca02317ddae46601338049d81bab4263
MunawarHussain-Enosis <munawar.hussain@enosisbd.com>
2019-05-31 15:18:59 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
+// Define our app...
+class App extends React.Component {
+  // Set the initial value when the app is first constructed.
+  state = {
+    value: initialValue,
+  }
+
+  // On change, update the app's React state with the new editor value.
+  onChange = ({ value }) => {
+    this.setState({ value })
+  }
+
+  // Render the editor.
+  render() {
+    return <div>
+        <Editor value={this.state.value} onChange={this.onChange} />
+      </div>
+  }
+}
+
+export default App;
